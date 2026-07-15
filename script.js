d45468ad4f717c34e7eb9e056cd63c496e268a1c
paulinemich <friedmanpauline@gmail.com>
2026-07-15 13:45:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+link.addEventListener('click', function(e) {
+e.preventDefault();
 
+const cible = document.querySelector(this.getAttribute('href'));
+
+if(cible){
+cible.scrollIntoView({
+behavior: "smooth"
+});
+}
+});
+});
+
+
+// Apparition des sections au
